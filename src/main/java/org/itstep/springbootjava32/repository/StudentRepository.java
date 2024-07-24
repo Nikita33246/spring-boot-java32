b41fa7d281230c9cded55cf2cfd838c06043b38d
LukashykYevhenii <lukashyk.yevhenii@chnu.edu.ua>
2024-07-24 21:37:59 +0300

    Academy v2

--- src/main/java/org/itstep/springbootjava32/repository/StudentRepository.java
+++ src/main/java/org/itstep/springbootjava32/repository/StudentRepository.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Repository;
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
 
+    Student findByName(String name);
 
+    Student findStudentByRating(int rating);
 
 }
---
+++ src/main/java/org/itstep/springbootjava32/service/DepartmentService.java
@@ -0,0 +1,25 @@
+package org.itstep.springbootjava32.service;
+
+
+import lombok.Setter;
+import org.itstep.springbootjava32.model.Department;
+import org.itstep.springbootjava32.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartmentService {
+
+    private DepartmentRepository departmentRepository;
+
+    @Autowired
+    public void setDepartmentRepository(DepartmentRepository departmentRepository) {
+            this.departmentRepository = departmentRepository;
+    }
+
+
+    public Department findDepartmentByStudentName(String studentName) {
+        return departmentRepository.findByStudentName(studentName);
+    }
+
+}
